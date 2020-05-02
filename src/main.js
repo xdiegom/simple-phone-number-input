75f0d72286d6cf21cba530bc8f58a028c01cd62b
Diego Melgar <diego.melgar.sv@gmail.com>
2020-05-02 00:16:08 -0600

    ✨Make component installable using Vue.use()

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 import PhoneNumberInput from "./components/PhoneNumberInput";
-export default PhoneNumberInput;
+
+export default {
+  // eslint-disable-next-line no-unused-vars
+  install(Vue, options) {
+    Vue.component("simple-phone-number-input", PhoneNumberInput);
+  }
+};
