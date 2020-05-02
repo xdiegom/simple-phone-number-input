dc5c774adacb440c8cf2d6f3e96f7cd5b4f9ac7a
Diego Melgar <diego.melgar.sv@gmail.com>
2020-05-01 18:08:48 -0600

    ✨ Base phone number input component

--- src/components/PhoneNumberInput.vue
+++ src/components/PhoneNumberInput.vue
@@ -1,13 +1,45 @@
 <template>
-  <div>
-    PhoneNumberInput component
+  <div class="phone_input__container">
+    <label class="phone_input__label">Phone number</label>
+    <div class="phone_input__input">
+      <select v-model="countryCode">
+        <option value="sv">El Salvador</option>
+      </select>
+      <span class="phone_input__dial_code">+503</span>
+      <input v-model="phoneNumberInput" type="text" />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "PhoneNumberInput"
+  name: "PhoneNumberInput",
+  data() {
+    return {
+      countryCode: "sv",
+      phoneNumberInput: null
+    };
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.phone_input__container {
+  display: flex;
+  flex-direction: column;
+}
+
+.phone_input__label {
+  font-weight: bold;
+  margin-bottom: 0.2rem;
+}
+
+.phone_input__input {
+  display: flex;
+  align-items: center;
+}
+
+.phone_input__dial_code {
+  margin: 0 0.2rem;
+}
+</style>
