f6cf37b9dfa2c25406fd48ea3f579c9a349f0442
Diego Melgar <diego.melgar.sv@gmail.com>
2020-05-01 20:32:16 -0600

    ✨📦 Add libphonenumber-js and v-mask

--- src/components/PhoneNumberInput.vue
+++ src/components/PhoneNumberInput.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="phone_input__container">
-    <label class="phone_input__label">Phone number</label>
+    <label class="phone_input__label">
+      <slot name="label">{{ labelTitle }}</slot>
+    </label>
     <div class="phone_input__input">
       <select class="phone_input__select" v-model="countryCode">
         <option
@@ -11,24 +13,58 @@
           {{ country.name }}
         </option>
       </select>
-      <span class="phone_input__dial_code">+503</span>
-      <input v-model="phoneNumberInput" type="text" />
+      <span class="phone_input__dial_code"> +{{ countryCallingCode }} </span>
+      <input
+        v-model="phoneNumberInput"
+        type="text"
+        v-mask="mask"
+        :placeholder="mask"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import allCountries from "../countries";
+import {
+  getExampleNumber,
+  parsePhoneNumberFromString
+} from "libphonenumber-js";
+import examples from "libphonenumber-js/examples.mobile.json";
+import translations from "../translations";
+import { VueMaskDirective } from "v-mask";
+
 export default {
   name: "PhoneNumberInput",
+  directives: {
+    mask: VueMaskDirective
+  },
   props: {
+    value: {
+      type: String,
+      default: null
+    },
     locale: {
       type: String,
-      default: "es"
+      default: "es",
+      validator: function(value) {
+        if (Object.keys(translations).indexOf(value) === -1) {
+          console.error(`Locale ${value} is not supported`);
+          return false;
+        }
+        return true;
+      }
     },
     defaultCountryCode: {
       type: String,
-      default: "sv"
+      default: "sv",
+      validator: function(value) {
+        const { es } = translations;
+        if (Object.keys(es.countries).indexOf(value) === -1) {
+          console.error(`Country code '${value}' is not supported`);
+          return false;
+        }
+        return true;
+      }
     }
   },
   data() {
@@ -40,15 +76,86 @@ export default {
   mounted() {
     this.countryCode = this.defaultCountryCode;
   },
+  watch: {
+    value(number) {
+      if (number) this.phoneNumberInput = number;
+    },
+    phoneNumberInput() {
+      this.format();
+    },
+    countryCode() {
+      this.format();
+    }
+  },
   computed: {
+    labelTitle() {
+      return translations[this.locale].label;
+    },
     countries() {
-      const countries = allCountries[this.locale];
+      const countries = translations[this.locale].countries;
       return Object.keys(countries).map(code => {
         return {
           code: code,
           name: countries[code]
         };
       });
+    },
+    countryCallingCode() {
+      if (this.phoneNumberExample)
+        return this.phoneNumberExample.countryCallingCode;
+
+      return "";
+    },
+    phoneNumberExample() {
+      const phoneNumber = this.countryCode
+        ? getExampleNumber(this.countryCode.toUpperCase(), examples)
+        : null;
+      return phoneNumber;
+    },
+    mask() {
+      const internationalPhoneNumberExample = this.phoneNumberExample
+        ? this.phoneNumberExample.formatInternational()
+        : null;
+      const nationalPhoneNumberExample = this.phoneNumberExample
+        ? this.phoneNumberExample.formatNational()
+        : null;
+
+      const phonePartMask = internationalPhoneNumberExample
+        ? internationalPhoneNumberExample.split(" ")
+        : [];
+
+      const mask =
+        phonePartMask.length > 1
+          ? nationalPhoneNumberExample.replace(/\d/g, "#")
+          : "";
+
+      return mask;
+    }
+  },
+  methods: {
+    format() {
+      const number = this.phoneNumberInput;
+      if (number) {
+        const phoneNumber = parsePhoneNumberFromString(
+          number,
+          this.countryCode.toUpperCase()
+        );
+        if (phoneNumber) {
+          if (phoneNumber.country !== this.countryCode) {
+            this.countryCode = phoneNumber.country.toLowerCase();
+          }
+          const formatted = {
+            phoneNumberExample: this.phoneNumberExample.nationalNumber,
+            countryCode: phoneNumber.country,
+            countryCallingCode: phoneNumber.countryCallingCode,
+            nationalNumber: phoneNumber.nationalNumber,
+            internationalNumber: phoneNumber.number,
+            isValid: phoneNumber.isValid()
+          };
+          this.$emit("onFormatted", formatted);
+        }
+      } else this.$emit("onFormatted", null);
+      this.$emit("input", number);
     }
   }
 };
---
+++ src/translations/index.js
@@ -0,0 +1,12 @@
+import countries from "../countries";
+
+export default {
+  es: {
+    label: "Número de teléfono",
+    countries: countries.es
+  },
+  en: {
+    label: "Phone number",
+    countries: countries.en
+  }
+};
